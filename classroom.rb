32aa12f100dae56fec097b2ef2dbdd02ca9d2615
Evans Sitibekiso <jevacrism@gmail.com>
2022-06-25 15:38:24 +0200

    Fix linter errors

--- classroom.rb
+++ classroom.rb
@@ -1,14 +1,13 @@
 class Classroom
-    attr_accessor :label, :students
-  
-    def initialize(lebal)
-      @lebal = lebal
-      @student = []
-    end
-  
-    def add_student(student)
-      @student.push(student)
-      student.classroom = self
-    end
+  attr_accessor :label, :students
+
+  def initialize(lebal)
+    @lebal = lebal
+    @student = []
   end
-  
+
+  def add_student(student)
+    @student.push(student)
+    student.classroom = self
+  end
+end
