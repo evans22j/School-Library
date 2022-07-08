702415f4f1b56b282bdfaacf8c2d7692dfb956de
Evans Sitibekiso <jevacrism@gmail.com>
2022-07-09 01:06:14 +0200

    nameable class

--- nameable.rb
+++ nameable.rb
@@ -15,14 +15,8 @@ class Decorator < Nameable
   end
 end
 
-class CapitalizeDecorator < Decorator
+class Nameable
   def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.slice(0, 10)
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
   end
 end
