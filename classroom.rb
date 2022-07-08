f4ca4b8d11f69309ec243958c81bace053839d7e
Evans Sitibekiso <jevacrism@gmail.com>
2022-07-09 00:52:13 +0200

    create classroom.rb

--- classroom.rb
+++ classroom.rb
@@ -1,13 +1,14 @@
 class Classroom
-  attr_accessor :label, :students
+  attr_accessor :label
+  attr_reader :students
 
   def initialize(label)
     @label = label
-    @student = []
+    @students = []
   end
 
   def add_student(student)
-    @student.push(student)
+    @students.push(student)
     student.classroom = self
   end
 end
