44cf0aab715a7637ec1a1dc030dca6d8998f76ea
Evans Sitibekiso <jevacrism@gmail.com>
2022-07-09 10:16:45 +0200

    fix linters

--- nameable.rb
+++ nameable.rb
@@ -3,20 +3,3 @@ class Nameable
     raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
   end
 end
-
-class Decorator < Nameable
-  def initialize(nameable)
-    @nameable = nameable
-    super()
-  end
-
-  def correct_name
-    @nameable.correct_name
-  end
-end
-
-class Nameable
-  def correct_name
-    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
-  end
-end
